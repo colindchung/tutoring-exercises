020d4c1b3d5574db03259a64d2a639876a5752e5
Colin Chung <colin.d.chung@gmail.com>
2021-12-11 21:30:37 -0500

    9-2

---
+++ exercises/9-2-greeting.py
@@ -0,0 +1,12 @@
+name = input("Hello, what is your name?\n")
+print("Nice to meet you " + name + "!")
+
+age = int(input("How old are you?\n"))
+my_age = 10
+age_difference = abs(my_age - age)
+print("We are " + str(age_difference) + " years apart!")
+
+favourite_food = input("What is your favourite food?")
+print(favourite_food + " sounds yummy!")
+
+print("Anyways, it was nice meeting you, " + name + ", let's talk again soon. Goodbye!")
